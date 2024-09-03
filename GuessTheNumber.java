df2dd932a9dca00fdfc08166ee9a3c0b3a51712b
TausifSS <tausifss015@gmail.com>
2024-09-03 13:11:33 +0530

    Update GuessTheNumber.java
    Adding new mini project

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,1 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Create a Random object to generate a random number
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1; // Random number between 1 and 100
+        int numberOfTries = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to Guess the Number!");
+        System.out.println("I have selected a number between 1 and 100. Try to guess it!");
+
+        // Main game loop
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
+            numberOfTries++;
+
+            if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the number in " + numberOfTries + " tries.");
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
 
